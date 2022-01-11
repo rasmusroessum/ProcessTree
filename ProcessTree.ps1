2b0082f820e11d84dcd82d7c8da4217a0c9a5c38
Rasmus Røssum <reachrasmus@gmail.com>
2022-01-11 15:35:42 +0100

    remove added process if closed

--- ProcessTree.ps1
+++ ProcessTree.ps1
@@ -1,4 +1,6 @@
-﻿[reflection.assembly]::LoadWithPartialName("System.Windows.Forms") |Out-Null
+﻿
+
+[reflection.assembly]::LoadWithPartialName("System.Windows.Forms") |Out-Null
 [reflection.assembly]::LoadWithPartialName("System.Drawing") |Out-Null
 $form = New-Object System.Windows.Forms.Form -Property @{
     Name = 'WinWat'
@@ -13,26 +15,26 @@ $tree = New-Object System.Windows.Forms.TreeView -Property @{
 $getProcess = { Get-CimInstance Win32_Process }
 $p = . $getProcess
 $count = $p.Count
-$p = $p |sort CreationDate |select Name, ProcessId, CreationDate, ParentProcessId, @{
+$p = $p |sort CreationDate |select Name, ProcessId, Path, CreationDate, ParentProcessId, @{
     N = 'ParentIsOpen'
     E = { [bool](Get-Process -Id $_.ParentProcessId -Ea SilentlyContinue) }
 }
-[void]$tree.Nodes.Add(0, ($p |where ProcessId -eq 0).Name)
+[void]$tree.Nodes.Add(0, (($p |where ProcessId -eq 0).Name + ': ' + ($p |where ProcessId -eq 0).Path))
 $p |where ProcessId -ne 0 |foreach {
     if($_.ParentProcessId -eq 0 -or $_.ParentIsOpen -eq 0){
-        [void]$tree.Nodes[0].Nodes.Add($_.ProcessId, $_.Name)
+        [void]$tree.Nodes[0].Nodes.Add($_.ProcessId, ($_.Name + ': ' + $_.Path))
     }else{
         $find = $tree.Nodes.Find($_.ParentProcessId, 1)
         if($find){
-            [void]$find[0].Nodes.Add($_.ProcessId, $_.Name)
+            [void]$find[0].Nodes.Add($_.ProcessId, ($_.Name + ': ' + $_.Path))
         }else{
-            $forLater += $_
+            [array]$forLater += $_
         }
     }
 }
 $forLater |foreach {
     $find = $tree.Nodes.Find($_.ParentProcessId, 1)
-    [void]$find[0].Nodes.Add($_.ProcessId, $_.Name)
+    [void]$find[0].Nodes.Add($_.ProcessId, ($_.Name + ': ' + $_.Path))
 }
 $form.Controls.Add($tree)
 function Enable-ProcessCreationEvent
@@ -53,21 +55,36 @@ function Disable-ProcessCreationEvent
    Unregister-Event -Force -SourceIdentifier "WMI.ProcessCreated"
 }
 Enable-ProcessCreationEvent
+function Show-Nodes($tree){
+    function Show-NodeCheckChild($tree){
+        if($tree.Nodes.Name){
+            $tree.Nodes.Name
+        }
+        If($tree.Nodes.Count -ge 1){
+            Show-Nodes $tree.Nodes
+        }
+    }
+    $array = [array]@()
+    Show-NodeCheckChild -tree $tree |foreach {
+        $array += [int]$_
+    }
+    $array
+}
 $timer.add_Tick({
     Get-Event |foreach {
         # Program opened
         $find = $tree.Nodes.Find($_.SourceArgs.ParentProcessId, 1)
-        $newNode = New-Object System.Windows.Forms.TreeNode -Property @{Text = $_.SourceArgs.ProcessName; Name = $_.SourceArgs.ProcessId}
+        $newNode = New-Object System.Windows.Forms.TreeNode -Property @{Text = ($_.SourceArgs.ProcessName + ': ' + $_.SourceArgs.Path); Name = $_.SourceArgs.ProcessId}
         $newNode.ForeColor = [System.Drawing.Color]::Green
         $find[0].Nodes.Add($newNode)
         $_ | Remove-Event
     }
-    $diff = diff (. $getProcess) $p -Property ProcessId
+    $diff = diff (. $getProcess).ProcessId (Show-Nodes -tree $tree)
     if($diff){
         $diff |foreach {
             if($_.SideIndicator -eq '=>'){
                 # Program closed
-                $find = $tree.Nodes.Find($_.ProcessId, 1)
+                $find = $tree.Nodes.Find($_.InputObject, 1)
                 $find[0].ForeColor = [System.Drawing.Color]::Red
             }
         }
@@ -78,3 +95,4 @@ $form.Add_Closing({
     Disable-ProcessCreationEvent
 })
 [void]$form.ShowDialog()
+
